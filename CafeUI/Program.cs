afc1043b778522aef6a3ff446669f849c9c231e6
Robert Darku <rdarku@gmail.com>
2020-10-01 23:24:19 -0400

    Komodo Cafe first push

--- CafeUI/Program.cs
+++ CafeUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CafeUI.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace CafeUI
     {
         static void Main(string[] args)
         {
+            MenuManager menuManager = new MenuManager();
+            menuManager.Run();
         }
     }
 }
